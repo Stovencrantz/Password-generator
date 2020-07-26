d12e9d418a73794ea5779fe78cc35761990f292c
Stovencrantz <sknapp143@gmail.com>
2020-07-26 10:48:05 -0400

    Re-organized file structure so that index.html will be displayed when website deployed on gitHub

--- Assets/Develop/script.js
+++ Assets/Develop/script.js
@@ -2,10 +2,8 @@ var lowerCaseGroup = "abcdefghijklmnopqrstuvwxyz";
 var upperCaseGroup = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var numGroup = "0123456789";
 var specCharGroup = "!@#$%^&*()?[]{}<>?/:;|~";
-var passwordLength = 0;
-var passwordArr = [];
-var passwordStr = "";
-var _password = "";
+
+
 
 console.log(lowerCaseGroup);
 console.log(upperCaseGroup);
@@ -13,6 +11,10 @@ console.log(numGroup);
 console.log(specCharGroup);
 
 function generatePassword(){
+  var passwordLength = 0;
+  var passwordArr = [];
+  var passwordStr = "";
+
   passwordLength = prompt("Please enter how long you would like your password to be: ");
   while (passwordLength < 8 || passwordLength > 128) {
     passwordLength = prompt("Sorry, that didnt quite work. Please enter the length of your password from 8-128 characters: ");
@@ -44,7 +46,7 @@ function generatePassword(){
   passwordArr = passwordStr.split("");
   console.log(passwordArr);
   console.log(passwordArr.length);
-  
+  var _password = "";
   for (var i = 0; i < passwordLength; i++){
     var char = Math.floor(Math.random()*passwordArr.length);  //get a random value between 0 and 84 
     _password = _password+passwordArr[char];
@@ -58,7 +60,7 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+ var password = generatePassword();
   console.log("Finalized password: " + password);
   var passwordText = document.querySelector("#password");
 
